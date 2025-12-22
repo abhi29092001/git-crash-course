e794e42a01e0f0df60eeb86c76e979e1a964f833
Abhishek Sharma <sharmaabh458@gmail.com>
2025-12-22 18:45:31 +0530

    Added age variable and age getter function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Sam");
+        Person person = new Person("Sam", "34");
         System.out.println(person.getName());
+        System.out.println(person.getAge());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,17 @@
 public class Person {
     private final String name;
+    private final String age;
 
-    public Person(String name) {
+    public Person(String name, String age) {
         this.name = name;
+        this.age = age;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getAge() {
+        return age;
     }
 }
