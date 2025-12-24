d46108227f87ac156f95b113d6659875a8ce1d85
Abhishek <97339585+abhi29092001@users.noreply.github.com>
2025-12-24 12:27:01 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Patrick", 34);
+        Person person = new Person("Patrick", 45);
         Student student = new Student("John", 15, "stu12826");
         person.displayInfo();
         student.displayInfo();
